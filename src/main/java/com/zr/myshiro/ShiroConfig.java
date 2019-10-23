d66873778aa40150727fef1cb26fbca2c080742d
周锐 <28391383+SingleRela@users.noreply.github.com>
2019-10-23 10:52:33 +0800

    优化代码风格

--- src/main/java/com/zr/myshiro/ShiroConfig.java
+++ src/main/java/com/zr/myshiro/ShiroConfig.java
@@ -2,11 +2,9 @@ package com.zr.myshiro;
 
 import org.apache.shiro.mgt.SessionStorageEvaluator;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
-import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.mgt.DefaultWebSessionStorageEvaluator;
-import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
