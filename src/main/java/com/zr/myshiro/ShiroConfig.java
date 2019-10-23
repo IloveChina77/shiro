e3d6afd7141cef582cfffd8ada34ac14cf85b5eb
周锐 <28391383+SingleRela@users.noreply.github.com>
2019-10-23 10:51:39 +0800

    找到进入不到doGetAuthorizationInfo方法的原因，注释掉了ShiroConfig 中一个方法

--- src/main/java/com/zr/myshiro/ShiroConfig.java
+++ src/main/java/com/zr/myshiro/ShiroConfig.java
@@ -14,13 +14,12 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- *  配置shiro
- *  note : SecurityManager in Shiro framework must be singleton whether static or not
- *
- *  Shiro 的核心部分是 SecurityManager
- *  它负责安全认证和授权。Shiro 本身已经实现了所有细节，用户完全可以把它当作一个黑盒来使用。
- *  SecurityUtils对象  本质上就是一个工厂类似Spring中的ApplicationContext
- *
+ * 配置shiro
+ * note : SecurityManager in Shiro framework must be singleton whether static or not
+ * <p>
+ * Shiro 的核心部分是 SecurityManager
+ * 它负责安全认证和授权。Shiro 本身已经实现了所有细节，用户完全可以把它当作一个黑盒来使用。
+ * SecurityUtils对象  本质上就是一个工厂类似Spring中的ApplicationContext
  */
 
 @Configuration
@@ -28,6 +27,7 @@ public class ShiroConfig {
 
     /**
      * 创建 MyRealm 实例
+     *
      * @reutrn the instance of class MyRealm
      */
     @Bean
@@ -37,6 +37,7 @@ public class ShiroConfig {
 
     /**
      * 构建 SecurityManager 环境
+     *
      * @return the instance of class DefaultWebSecurity
      */
     @Bean
@@ -48,7 +49,6 @@ public class ShiroConfig {
 
     /**
      * lifecycleBeanPostProcessor
-     *
      */
     @Bean
     public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
@@ -57,25 +57,26 @@ public class ShiroConfig {
 
     /**
      * 创建ShiroFilterFactoryBean 的实例 交由 Spring 管理
+     *
      * @reutrn
      */
     @Bean
     ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager defaultWebSecurityManager) {
-       // ShiroFilterFactoryBean 应该是集合了 Filter 和 Factory 的功能
+        // ShiroFilterFactoryBean 应该是集合了 Filter 和 Factory 的功能
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-       // 指定SecurityManager
+        // 指定SecurityManager
         shiroFilterFactoryBean.setSecurityManager(defaultWebSecurityManager);
-       // 指定 登录url
+        // 指定 登录url
         shiroFilterFactoryBean.setLoginUrl("/index.html");
-       // 指定登录成功页面
+        // 指定登录成功页面
         shiroFilterFactoryBean.setSuccessUrl("/index");
-       // 指定
+        // 指定
         shiroFilterFactoryBean.setUnauthorizedUrl("/unauthorizedUrl");
 
         // 指定 路径过滤规则
         Map<String, String> map = new LinkedHashMap<>();
         // 放行一些请求
-        map.put("/static/index.html","anon");
+        map.put("/static/index.html", "anon");
         map.put("/doLogin", "anon");
         map.put("/swagger**/**", "anon");
         map.put("/webjars/**", "anon");
@@ -95,7 +96,7 @@ public class ShiroConfig {
     SessionStorageEvaluator sessionStorageEvaluator() {
         DefaultWebSessionStorageEvaluator sessionStorageEvaluator = new DefaultWebSessionStorageEvaluator();
         sessionStorageEvaluator.setSessionStorageEnabled(false);
-        return  sessionStorageEvaluator;
+        return sessionStorageEvaluator;
     }
 
 /*    @Bean
@@ -104,8 +105,6 @@ public class ShiroConfig {
         defaultAdvisorAutoProxyCreator.setUsePrefix(true);
         return defaultAdvisorAutoProxyCreator;
     }*/
-
-
 
 
 }
